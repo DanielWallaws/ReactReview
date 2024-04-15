e6da8fc54e45645b6bdd1db3f45e40ddb0ab17f4
Daniel Wallaws Borges De Oliveira <daniel.oliveira@seduh.df.gov.br>
2024-04-15 11:13:47 -0300

    sending until class 17 module 3 - Exercices

--- iniciando-projeto/primeiro-next/src/app/page.tsx
+++ iniciando-projeto/primeiro-next/src/app/page.tsx
@@ -1,54 +1,217 @@
 "use client"
 
-import { ToDoItem } from "@/types/ToDoItem";
-import { useState } from "react";
-
-
-//Aula Arrays em States - Excluir - Modulo 3 aula 14
+//Exercíos - Módulo 3 aula 17
 const Page = ()=> {
-  const [itemInput, setitemInput] = useState('');
-  const [list, setList] = useState<ToDoItem[]>([
-    { label: 'Fazer deve de casa', checked: false },
-    { label: 'Comprar o bolo', checked: false }
-  ]);
-  const handleAddButton = ()=> {
-    if(itemInput.trim() === '') return;
-    setList([...list, {label: itemInput, checked: false}])
-    setitemInput('')
-  };
-  const deleteItem = (index: number) => {
-    setList(list.filter((item, key)=> key !== index))
-  }
+  
+  
 
 
 return (
    
   
     <div className="w-screen h-screen flex flex-col justify-center items-center text-2xl">
-     <h1 className="text-4xl mt-5">Lista de Tarefas</h1>
-      <div className="flex w-full max-w-lg my-3 p-4 rounded-md gb-gray-700 border-2 border-gray-500 bg-gray-600">
-        <input 
-        type="text" 
-        placeholder="O que deseja fazer"
-        className="flex-1 border-black p-3 pr-2 text-2xl text-black rounded-md mr-3" 
-        value={itemInput}
-        onChange={e => setitemInput(e.target.value)}
-        />
-        <button onClick={handleAddButton}>Adicionar</button>
-      </div>
-
-      <p className="my-4">{list.length} Itens na lista</p>
-
-      <ul className="w-full max-w-lg list-disc pl-5 ">
-        {list.map((item, index) => (
-          <li>{item.label} - <button className="hover:underline" onClick={()=> deleteItem(index)}>[ deletar ]</button></li>
-        ))}
-      </ul>
+    
     </div>
 )}
 
 
  export default Page;
+
+// "use client"
+
+// import { ToDoItem } from "@/types/ToDoItem";
+// import { useState } from "react";
+
+
+// //Aula Arrays em States - Usando ID - Modulo 3 aula 16
+// const Page = ()=> {
+//   const [itemInput, setitemInput] = useState('');
+//   const [list, setList] = useState<ToDoItem[]>([
+//     {id: 1, label: 'Fazer deve de casa', checked: false },
+//     {id: 2, label: 'Comprar o bolo', checked: false }
+//   ]);
+
+//   const handleAddButton = ()=> {
+//     if(itemInput.trim() === '') return;
+//     setList([...list, {id: list.length+1 ,label: itemInput, checked: false}])
+//     setitemInput('')
+//   };
+
+//   const deleteItem = (id: number) => {
+//     setList(list.filter((item)=> item.id !== id))
+//   }
+
+//   const toggleItem = (id: number)=> {
+//     let newList = [...list];
+
+//     for(let i in newList) {
+//       if(newList[i].id === id) {
+//         newList[i].checked = !newList[i].checked;
+//       }
+//     }
+    
+//     setList(newList)
+//   }
+  
+
+
+// return (
+   
+  
+//     <div className="w-screen h-screen flex flex-col justify-center items-center text-2xl">
+//      <h1 className="text-4xl mt-5">Lista de Tarefas</h1>
+//       <div className="flex w-full max-w-lg my-3 p-4 rounded-md gb-gray-700 border-2 border-gray-500 bg-gray-600">
+//         <input 
+//         type="text" 
+//         placeholder="O que deseja fazer"
+//         className="flex-1 border-black p-3 pr-2 text-2xl text-black rounded-md mr-3" 
+//         value={itemInput}
+//         onChange={e => setitemInput(e.target.value)}
+//         />
+//         <button onClick={handleAddButton}>Adicionar</button>
+//       </div>
+
+//       <p className="my-4">{list.length} Itens na lista</p>
+
+//       <ul className="w-full max-w-lg list-disc pl-5 ">
+//         {list.map((item) => (
+//           <li key={item.id}>
+//             <input onClick={()=>toggleItem(item.id) } type="checkbox" checked={item.checked} className="w-5 h-5 mr-3"/>
+//           {item.label} - <button className="hover:underline" onClick={()=> deleteItem(item.id)}>[ deletar ]</button></li>
+//         ))}
+//       </ul>
+//     </div>
+// )}
+
+
+//  export default Page;
+
+// "use client"
+
+// import { ToDoItem } from "@/types/ToDoItem";
+// import { useState } from "react";
+
+
+// //Aula Arrays em States - ALterar - Modulo 3 aula 15
+// const Page = ()=> {
+//   const [itemInput, setitemInput] = useState('');
+//   const [list, setList] = useState<ToDoItem[]>([
+//     { label: 'Fazer deve de casa', checked: false },
+//     { label: 'Comprar o bolo', checked: false }
+//   ]);
+
+//   const handleAddButton = ()=> {
+//     if(itemInput.trim() === '') return;
+//     setList([...list, {label: itemInput, checked: false}])
+//     setitemInput('')
+//   };
+
+//   const deleteItem = (index: number) => {
+//     setList(list.filter((item, key)=> key !== index))
+//   }
+
+//   //2ª forma de resolver
+//   const toggleItem = (index: number)=> {
+//     let newList = [...list];
+//     newList[index].checked = !newList[index].checked;
+    
+//     setList(newList)
+//   }
+
+//   //1ª forma de resolver
+//   // const toggleItem = (index: number)=> {
+//   //   let newList = [...list];
+//   //   for(let i in newList) {
+//   //     if(index === parseInt(i)) {
+//   //       newList[i].checked = !newList[i].checked;
+//   //     }
+//   //   }
+//   //   setList(newList)
+//   // }
+
+
+// return (
+   
+  
+//     <div className="w-screen h-screen flex flex-col justify-center items-center text-2xl">
+//      <h1 className="text-4xl mt-5">Lista de Tarefas</h1>
+//       <div className="flex w-full max-w-lg my-3 p-4 rounded-md gb-gray-700 border-2 border-gray-500 bg-gray-600">
+//         <input 
+//         type="text" 
+//         placeholder="O que deseja fazer"
+//         className="flex-1 border-black p-3 pr-2 text-2xl text-black rounded-md mr-3" 
+//         value={itemInput}
+//         onChange={e => setitemInput(e.target.value)}
+//         />
+//         <button onClick={handleAddButton}>Adicionar</button>
+//       </div>
+
+//       <p className="my-4">{list.length} Itens na lista</p>
+
+//       <ul className="w-full max-w-lg list-disc pl-5 ">
+//         {list.map((item, index) => (
+//           <li style={{ listStyleType: 'none'}}>
+//             <input onClick={()=>toggleItem(index) } type="checkbox" checked={item.checked} className="w-5 h-5 mr-3"/>
+//           {item.label} - <button className="hover:underline" onClick={()=> deleteItem(index)}>[ deletar ]</button></li>
+//         ))}
+//       </ul>
+//     </div>
+// )}
+
+
+//  export default Page;
+
+// "use client"
+
+// import { ToDoItem } from "@/types/ToDoItem";
+// import { useState } from "react";
+
+
+// //Aula Arrays em States - Excluir - Modulo 3 aula 14
+// const Page = ()=> {
+//   const [itemInput, setitemInput] = useState('');
+//   const [list, setList] = useState<ToDoItem[]>([
+//     { label: 'Fazer deve de casa', checked: false },
+//     { label: 'Comprar o bolo', checked: false }
+//   ]);
+//   const handleAddButton = ()=> {
+//     if(itemInput.trim() === '') return;
+//     setList([...list, {label: itemInput, checked: false}])
+//     setitemInput('')
+//   };
+//   const deleteItem = (index: number) => {
+//     setList(list.filter((item, key)=> key !== index))
+//   }
+
+
+// return (
+   
+  
+//     <div className="w-screen h-screen flex flex-col justify-center items-center text-2xl">
+//      <h1 className="text-4xl mt-5">Lista de Tarefas</h1>
+//       <div className="flex w-full max-w-lg my-3 p-4 rounded-md gb-gray-700 border-2 border-gray-500 bg-gray-600">
+//         <input 
+//         type="text" 
+//         placeholder="O que deseja fazer"
+//         className="flex-1 border-black p-3 pr-2 text-2xl text-black rounded-md mr-3" 
+//         value={itemInput}
+//         onChange={e => setitemInput(e.target.value)}
+//         />
+//         <button onClick={handleAddButton}>Adicionar</button>
+//       </div>
+
+//       <p className="my-4">{list.length} Itens na lista</p>
+
+//       <ul className="w-full max-w-lg list-disc pl-5 ">
+//         {list.map((item, index) => (
+//           <li>{item.label} - <button className="hover:underline" onClick={()=> deleteItem(index)}>[ deletar ]</button></li>
+//         ))}
+//       </ul>
+//     </div>
+// )}
+
+
+//  export default Page;
 
 // "use client"
 
